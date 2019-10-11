01ffba1a2061d941d99cad6cf7044e12b5d1be39
Kelvin Oliveira <kelvinodias@gmail.com>
2019-10-11 10:49:52 -0300

    Criação do Modelo e Instalação do Template Codebase

---
+++ Modelo/Entidades/Backup.cs
@@ -0,0 +1,15 @@
+﻿using Modelo.Entidades.Enumeradores;
+using System;
+
+namespace Modelo.Entidades
+{
+    public class Backup
+    {
+        public long? BackupId { get; set; }
+        public DateTime DataHora { get; set; }
+        public DateTime DataHoraExecucao { get; set; }
+        public Periodo PeriodoExecucao { get; set; }
+        public string Assinatura { get; set; }
+        public BancoDeExecucao DestinoGravacao { get; set; }
+    }
+}
--- Persistencia/Context/EFContext.cs
+++ Persistencia/Context/EFContext.cs
@@ -1,20 +1,27 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Modelo.Entidades;
 
 namespace Persistencia.Context
 {
     public class EFContext : DbContext
     {
+        public DbSet<Backup> Backups { get; set; }
+        public DbSet<Contato> Contatos { get; set; }
+        public DbSet<Rotina> Rotinas { get; set; }
+        public DbSet<Servidor> Servidores { get; set; }
+        public DbSet<Ocorrencia> Ocorrencias { get; set; }
+        public DbSet<OcorrenciaIteracao> OcorrenciaIteracoes { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Fadami\source\repos\ControleOperacional\ControleOperacional\App_Data\CONTROLE_OPERACIONAL.mdf;Integrated Security=True");
         }
-
-
     }
+
 }
+
+
+
+
+
 
