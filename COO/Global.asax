3621ef5ff09e98d8672f49acb55bbdcf8b7a7b8b
Kelvin Oliveira <kelvinodias@gmail.com>
2019-10-10 22:24:18 -0300

    Adicionar arquivos de projeto.

---
+++ COO/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="COO.MvcApplication" Language="C#" %>
---
+++ Persistencia/Context/EFContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia.Context
+{
+    public class EFContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+
+    }
+}
+
